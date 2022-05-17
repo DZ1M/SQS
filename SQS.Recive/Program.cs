9ca77136c371ae123b1ba5a951c7c829d93d9b73
Richard Trage <richard.trage@laborit.com.br>
2022-05-17 18:54:31 -0300

    Adicionar arquivos de projeto.

---
+++ SQS.Recive/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using Amazon.SQS;
+using Amazon.SQS.Model;
+
+Console.WriteLine("Recive - SQS");
+
+var client = new AmazonSQSClient(Amazon.RegionEndpoint.USEast1);
+var url = $"https://sqs.us-east-1.amazonaws.com/296256985399/TestSQS";
+var request = new ReceiveMessageRequest(url);
+
+while (true)
+{
+    var messages = await client.ReceiveMessageAsync(request);
+    foreach (var item in messages.Messages)
+    {
+        Console.WriteLine(item.Body);
+        await client.DeleteMessageAsync(url, item.ReceiptHandle);
+    }
+}
---
+++ SQS.Send/Program.cs
@@ -0,0 +1,12 @@
+﻿using Amazon.SQS;
+using Amazon.SQS.Model;
+
+Console.WriteLine("Send - SQS");
+
+var client = new AmazonSQSClient(Amazon.RegionEndpoint.USEast1);
+
+var url = "https://sqs.us-east-1.amazonaws.com/296256985399/TestSQS";
+var body = "Cotoco";
+var request = new SendMessageRequest(url, body);
+
+await client.SendMessageAsync(request);
